2a9369a9d6922c1f01a9d0c3ea3ec49420ceac2f
Mihir L. Agrawal <mihiragrawal022@gmail.com>
2022-08-24 19:37:38 +0530

    stdin input working feature and small fixes

--- src/Container/DockerContainer.php
+++ src/Container/DockerContainer.php
@@ -45,7 +45,7 @@ class DockerContainer{
       }
       //writing input in file
       if(!fwrite($file,$this->stdin)){
-        throw new \Exception('Internal Server Error');
+        throw new \Exception('PARAMETER INPUT IS EMPTY');
       }
       fclose($file);
       $file=null;
@@ -55,10 +55,7 @@ class DockerContainer{
     }
 
     private function prepareContainer() : string{
-      /** TODO - prepend the command timeout in below bashstmt
-       * timeout -s SIGKILL 3 
-       */
-      $bashstmt = 'docker run --rm -d -it -v '."\"".VOLUME_PATH.'/'.$this->folderName.':/'.$this->folderName."\"".' '.
+      $bashstmt = 'timeout -s SIGKILL 3 docker run --rm -d -it -v '."\"".VOLUME_PATH.'/'.$this->folderName.':/'.$this->folderName."\"".' '.
       DOCKER_IMAGE. ' bash '. $this->folderName . '/runner.sh ' . 
       " $this->timeout $this->folderName $this->compilerName $this->fileName.$this->languageExtension";
       if(!is_null($this->outputCommand)){
@@ -73,6 +70,7 @@ class DockerContainer{
 
       //error if occurs then should be redirect to log file in src/Container/errorlogs.txt
       $bashstmt = $this->prepareContainer() .' 2>>'. PARENT_FOLDER_PATH . '/src/Container/errorlogs.txt';
+      
       //get output container id to variable(in case of successful docker container running)
       $containerId = exec($bashstmt);
 
@@ -84,17 +82,6 @@ class DockerContainer{
         //This is such a big failure so to overcome that simple docker wait command blocks the terminal until 
         //the docker container is destroyed which helps us to make response synchronously after docker processing
         exec('docker wait '.$containerId);
-
-        //!! 
-        //TO-DO : Save the container id with timestamp and more info about folder made and their files in some no-sql database 
-        
-        
-        //TO-DO : replace all the files deletion jobs of php with cron jobs of bash with bash scripts  
-              
-        //after all the work delete the folder
-        // unlink(PARENT_FOLDER_PATH."/tmp/$this->folderName/tmp/$this->fileName.$this->languageExtension");
-        // unlink(PARENT_FOLDER_PATH."/tmp/$this->folderName/tmp/$this->fileName.$this->languageExtension");
-        // rmdir(PARENT_FOLDER_PATH.'/tmp/'.$this->folderName.'/tmp');
       }
       else{
         throw new \Exception('Internal Server Error');
